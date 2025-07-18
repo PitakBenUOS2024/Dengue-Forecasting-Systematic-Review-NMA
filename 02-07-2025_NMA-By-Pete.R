51ffc15d1e914ef58d420336d50c6c1fc9e7e81c
PitakBenUOS2024 <pbenjarattanaporn1@sheffield.ac.uk>
2025-07-18 08:56:10 +0100

    Update & Create NMA
    Update:
    NMA by Pete:  created sensitivity analysis on High quality studies, and trt_effects = 'fixed'.
    
    Create:
    new file "NMA Sub group by time horizon" which group into 3 range; short, medium, and long, following " Soyiri and Reidpath, 2013"

--- 02-07-2025_NMA-By-Pete.R
+++ 02-07-2025_NMA-By-Pete.R
@@ -3,9 +3,13 @@
 ## =====================
 library(data.table)
 library(multinma)
+library(ggplot2)
+library(readr)
 
 # getwd()
 # setwd('X:\\HAR_WG\\WG\\UKSEA_VAXHUB\\Systematic_Review_Dengue_Forecasting')
+
+# --- 0. Set up some data ---
 
 ## Pitak's last sheet with rankings
 D <- fread("PME.csv")
@@ -32,10 +36,8 @@ names(DR)[2] <- "method"
 DR
 
 
-
-
-# rmv <- c("Hasan, 2024") #single arm
-# DR <- DR[!Identifier %in% rmv]
+rmv <- c("Hasan, 2024") #single arm
+DR <- DR[!Identifier %in% rmv]
 DR[,summary(RMSE_log10)]
 DR[,err:=3 + RMSE_log10]
 
@@ -43,13 +45,17 @@ DM <- set_agd_arm( data= DR,
                    study = Identifier,
                    trt = method,
                    y=err,
-                   se=err/10
+                   se=err/10,
+                   trt_ref = "Naïve" # <<< THIS IS REFERENCE MODEL. Naïve beacause it's Most Basic Model
 )
 
 print(DM)
 
 plot(DM)
 
+
+
+# --- 1. Base Case NMA Setup ---
 # Take sometimes to run (e.g., 30 min)
 smkfit <- nma(DM,
               trt_effects = "random",
@@ -59,6 +65,7 @@ smkfit <- nma(DM,
               iter = 4000 # Increase from default (often 2000) to 4000, 6000, or more
               )
 
+saveRDS(smkfit, file = "base_case_NMA.rds")
 
 ## relative effects
 smk_releff <- relative_effects(smkfit, all_contrasts = FALSE)
@@ -67,14 +74,82 @@ plot(smk_releff, ref_line = 0)
 # ggsave("nma_releff.pdf",w=10,h=15)
 
 ## rank visualizations
-#Adjust Rank only 1 to 10
-smk_rankprobs <- posterior_rank_probs(smkfit, lower_better = TRUE)
-smk_rankprobs 
-plot(smk_rankprobs) + xlim(1, 47)
-# ggsave("nma_rnkprb.pdf",w=10,h=10)
+ 
+# smk_rankprobs <- posterior_rank_probs(smkfit, lower_better = TRUE)
+# smk_rankprobs 
+# plot(smk_rankprobs) + xlim(1, 47)#Adjust Rank
+# # ggsave("nma_rnkprb.pdf",w=10,h=10)
 
 
 smk_cumrankprobs <- posterior_rank_probs(smkfit, lower_better = TRUE, cumulative = TRUE)
 smk_cumrankprobs
-plot(smk_cumrankprobs) + xlim(1, 47)
+plot(smk_cumrankprobs) + xlim(1, 47)#Adjust Rank
 # ggsave("nma_crnkprb.pdf",w=10,h=10)
+
+
+# Sensitivity Analysis 
+
+# --- 2.Filter out Low QA studies Setup ---
+
+# Filter out Low and Medium quality (n=24)
+L_n_M_rmv <- c("Anggraeni, 2021", "Chakraborty, 2020", "Doni, 2020", "Jayashree, 2015", "Kerdprasop, 2020", "Mahdiana, 2017", "Nabilah, 2023", "Prome, 2024", "Shashvat, 2023", "Guo, 2018", "Ho, 2015", "Juraphanthong, 2021", "Khaira, 2020", "Ligue, 2022", "Liu, 2019", "Mustaffa, 2024", "Othman, 2022", "Pham, 2018", "Rajendran, 2023", "Shashvat, 2019", "Tian, 2024", "Weng, 2024", "Zhao, 2020", "Rangarajan, 2019") #single arm
+my_data_no_l_n_m_study <- DR[!Identifier %in% L_n_M_rmv]
+
+net_high_Q_studies <- set_agd_arm( data= my_data_no_l_n_m_study,
+                   study = Identifier,
+                   trt = method,
+                   y=err,
+                   se=err/10,
+                   trt_ref = "Naïve" # <<< THIS IS REFERENCE MODEL. Naïve beacause it's Most Basic Model
+)
+
+print("--- Network excluding Low and Medium qulity studies ---")
+print(net_high_Q_studies)
+plot(net_high_Q_studies)
+
+fit_high_Q_studies <- nma(net_high_Q_studies,
+                            trt_effects = "random",
+                            prior_intercept = normal(scale = 100),
+                            prior_trt = normal(scale = 100),
+                            prior_het = normal(scale = 5),
+                            iter = 4000 # Increase from default (often 2000) to 4000, 6000, or more
+)
+
+
+saveRDS(fit_high_Q_studies, file = "high_quality_case_NMA.rds")
+
+
+# Compare results
+print("--- Results without Low and Medium qulity studies ---")
+print(relative_effects(fit_high_Q_studies))
+plot(relative_effects(fit_high_Q_studies))
+
+print("--- SUCRA Ranks without Low and Medium qulity studies ---")
+print(posterior_rank_probs(fit_high_Q_studies, lower_better = TRUE, cumulative = TRUE))
+plot(posterior_rank_probs(fit_high_Q_studies, lower_better = TRUE, cumulative = TRUE)) + xlim(1, 39)
+
+
+# --- 3.Fixed Effects Setup ---
+
+fit_fixed <- nma(DM,
+                 trt_effects = "fixed",
+                 prior_intercept = normal(scale = 100),
+                 prior_trt = normal(scale = 100),
+                 prior_het = normal(scale = 5),
+                 iter = 4000 # Increase from default (often 2000) to 4000, 6000, or more
+)
+
+saveRDS(fit_fixed, file = "fixed_effect_case_NMA.rds")
+
+
+print("--- Fixed Effects Model Results ---")
+print(relative_effects(fit_fixed,))
+plot(relative_effects(fit_fixed))
+
+# print("--- Fixed Effects SUCRA Ranks ---")
+# print(posterior_rank_probs(fit_fixed))
+# plot(posterior_rank_probs(fit_fixed, lower_better = TRUE))
+
+print("--- Fixed Effects Cumulative Ranks ---")
+print(posterior_rank_probs(fit_fixed), cumulative = TRUE)
+plot(posterior_rank_probs(fit_fixed, lower_better = TRUE, cumulative = TRUE)) + xlim(1, 47)
---
+++ 16-07-2025_NMA_Sub_group_by_time_horizon.R
@@ -0,0 +1,185 @@
+# make sub-group by time horizon (short,medium,long range)
+# Use Soyiri and Reidpath, 2013 as the ref.
+
+
+library(dplyr)
+
+unique(D$`Time Horizon (Months)`)
+
+# Assuming D is your data frame/data.table
+
+# 1. Clean and convert 'Time Horizon (Months)' to numeric
+D <- D %>%
+  mutate(
+    Time_Horizon_Months_Numeric = as.numeric(
+      case_when(
+        `Time Horizon (Months)` == "not clear" ~ NA_real_, # Convert "not clear" to NA
+        TRUE ~ as.numeric(`Time Horizon (Months)`) # Convert other values to numeric
+      )
+    )
+  )
+
+
+# 2. Create D_short_range dataframe
+D_short_range <- D %>%
+  filter(
+    !is.na(Time_Horizon_Months_Numeric) &
+      Time_Horizon_Months_Numeric >= (1/30.44) & # Approximately 1 day
+      Time_Horizon_Months_Numeric <= 3           # Up to 3 months (quarter of a year)
+  )
+
+# 3. Create D_medium_range dataframe
+D_medium_range <- D %>%
+  filter(
+    !is.na(Time_Horizon_Months_Numeric) &
+      Time_Horizon_Months_Numeric > 3 &    # Greater than 3 months
+      Time_Horizon_Months_Numeric <= 12    # Up to 12 months (a year)
+  )
+
+# 4. Create D_long_range dataframe
+D_long_range <- D %>%
+  filter(
+    !is.na(Time_Horizon_Months_Numeric) &
+      Time_Horizon_Months_Numeric > 12 &   # Greater than 12 months
+      Time_Horizon_Months_Numeric <= 60    # Up to 60 months (five years)
+  )
+
+
+head(D_short_range)
+head(D_medium_range)
+head(D_long_range)
+
+
+
+
+
+
+
+(nmz <- names(D))
+(keep <- nmz[c(3,5,29)])
+
+#Plots agd arm
+
+#setup D_short_range df
+DR_short_range <- D_short_range[,..keep]
+names(DR_short_range)[2] <- "method"
+
+
+rmv <- c("Hasan, 2024") #single arm
+DR_short_range <- DR_short_range[!Identifier %in% rmv]
+DR_short_range[,summary(RMSE_log10)]
+DR_short_range[,err:=3 + RMSE_log10]
+
+
+DM_short_range <- set_agd_arm( data= DR_short_range,
+                   study = Identifier,
+                   trt = method,
+                   y=err,
+                   se=err/10
+                   ) 
+print(DM_short_range)
+
+plot(DM_short_range)
+
+
+#setup D_medium_range df
+DR_medium_range <- D_medium_range[,..keep]
+names(DR_medium_range)[2] <- "method"
+
+
+rmv <- c("Hasan, 2024") #single arm
+DR_medium_range <- DR_medium_range[!Identifier %in% rmv]
+DR_medium_range[,summary(RMSE_log10)]
+DR_medium_range[,err:=3 + RMSE_log10]
+
+
+DM_medium_range <- set_agd_arm( data= DR_medium_range,
+                               study = Identifier,
+                               trt = method,
+                               y=err,
+                               se=err/10
+) 
+
+print(DM_medium_range)
+
+plot(DM_medium_range)
+
+#setup D_long_range df
+DR_long_range <- D_long_range[,..keep]
+names(DR_long_range)[2] <- "method"
+
+
+rmv <- c("Hasan, 2024") #single arm
+DR_long_range <- DR_long_range[!Identifier %in% rmv]
+DR_long_range[,summary(RMSE_log10)]
+DR_long_range[,err:=3 + RMSE_log10]
+
+
+DM_long_range <- set_agd_arm( data= DR_long_range,
+                                study = Identifier,
+                                trt = method,
+                                y=err,
+                                se=err/10
+) 
+
+print(DM_long_range)
+
+plot(DM_long_range)
+
+
+#fit short range horizon
+
+fit_short_range <- nma(DM_short_range,
+              trt_effects = "fixed",
+              prior_intercept = normal(scale = 100),
+              prior_trt = normal(scale = 100),
+              prior_het = normal(scale = 5),
+              iter = 4000 # Increase from default (often 2000) to 4000, 6000, or more
+)
+
+print("--- Short range Effects Model Results  ---")
+print(relative_effects(fit_short_range,))
+plot(relative_effects(fit_short_range))
+
+print("--- Short range Effects Cumulative Ranks ---")
+print(posterior_rank_probs(fit_short_range), cumulative = TRUE)
+plot(posterior_rank_probs(fit_short_range, lower_better = TRUE, cumulative = TRUE)) + xlim(1, 10)
+
+
+#fit medium range horizon
+
+fit_medium_range <- nma(DM_medium_range,
+                       trt_effects = "fixed",
+                       prior_intercept = normal(scale = 100),
+                       prior_trt = normal(scale = 100),
+                       prior_het = normal(scale = 5),
+                       iter = 4000 # Increase from default (often 2000) to 4000, 6000, or more
+)
+
+print("--- medium range Effects Model Results  ---")
+print(relative_effects(fit_medium_range,))
+plot(relative_effects(fit_medium_range))
+
+print("--- medium range Effects Cumulative Ranks ---")
+print(posterior_rank_probs(fit_medium_range), cumulative = TRUE)
+plot(posterior_rank_probs(fit_medium_range, lower_better = TRUE, cumulative = TRUE)) + xlim(1, 31)
+
+
+#fit long range horizon
+
+fit_long_range <- nma(DM_long_range,
+                        trt_effects = "fixed",
+                        prior_intercept = normal(scale = 100),
+                        prior_trt = normal(scale = 100),
+                        prior_het = normal(scale = 5),
+                        iter = 4000 # Increase from default (often 2000) to 4000, 6000, or more
+)
+
+print("--- long range Effects Model Results  ---")
+print(relative_effects(fit_long_range,))
+plot(relative_effects(fit_long_range))
+
+print("--- long range Effects Cumulative Ranks ---")
+print(posterior_rank_probs(fit_long_range), cumulative = TRUE)
+plot(posterior_rank_probs(fit_long_range, lower_better = TRUE, cumulative = TRUE)) + xlim(1, 20)
+
